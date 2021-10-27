ecb0cf12db8a130d413a8d19833fb4057caccc15
Александр Артемов <aleksandr.artemov@outlook.com>
2021-10-27 01:24:02 -0700

    fix

--- ArtDevSitePage/Layouts/ArtDevSitePage/React/public/header.bundle.js
+++ ArtDevSitePage/Layouts/ArtDevSitePage/React/public/header.bundle.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),l=t(2);function c(){return r.a.createElement(r.a.Fragment,null,"Hello1")}t.n(l).a.render(r.a.createElement(c,null),document.getElementById("header"))}},[[3,1,2]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),l=t(2);function c(){return r.a.createElement(r.a.Fragment,null,"Hello1223")}t.n(l).a.render(r.a.createElement(c,null),document.getElementById("header"))}},[[3,1,2]]]);
 //# sourceMappingURL=header.bundle.js.map
--- ArtDevSitePage/Layouts/ArtDevSitePage/React/src/header.js
+++ ArtDevSitePage/Layouts/ArtDevSitePage/React/src/header.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 function Header() {	
 	return (
-		<>Hello1</>
+		<>Hello1223</>
 	)
 }
 
